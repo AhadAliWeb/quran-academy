51278ed9fc41182b2115df824b84043c15575227
Muhammad Ali <muhammadaliweb4@gmail.com>
2025-10-04 20:03:55 +0500

    Fixed Production Frontend

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,10 @@ app.use('/api/v1/lesson', lessonRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
+
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   );
 } else {
